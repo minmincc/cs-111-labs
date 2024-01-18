194867c4eff4ea10e9e4bb6142f320fc1ec87a88
Mingqi Zhao <mingqizhao@Mingqis-MacBook-Air.local>
2024-01-18 14:15:48 -0800

    fix code

--- cs111-lab0-source-code/proc_count.c
+++ cs111-lab0-source-code/proc_count.c
@@ -14,7 +14,9 @@ static int proc_count(struct seq_file *m, void *v){
 
     // Iterate through all the processes and increment the count
     for_each_process(task) {
-        process_count++;
+        if (task->state == TASK_RUNNING || task->state == TASK_INTERRUPTIBLE || task->state == TASK_UNINTERRUPTIBLE) {
+            process_count++;
+        }
     }
 
     // Write the number of processes to the seq_file
