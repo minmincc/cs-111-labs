dc58126a5de21eeb35672a85f63d70da72a5b9da
Mingqi Zhao <mingqizhao@Mingqis-MacBook-Air.local>
2024-01-30 21:54:03 -0800

    test

--- cs111-lab1/pipe.c
+++ cs111-lab1/pipe.c
@@ -5,64 +5,85 @@
 #include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
+    if (argc <= 1) {
         // If no command is passed, exit with EINVAL
         fprintf(stderr, "Usage: %s <command> [<command> ...]\n", argv[0]);
-        return EINVAL;
+        exit(EINVAL);
     }
 
-    if (argc == 2) { // If only one command is passed, execute it directly
-        execlp(argv[1], argv[1], NULL);
-        perror("execlp");
-        return EXIT_FAILURE;
-    }
-
-    int pipefds[2 * (argc - 2)]; // Array to hold the file descriptors for pipes
-    int pid;
-
-    // Create pipes
-    for (int i = 0; i < (argc - 2); i++) {
-        if (pipe(pipefds + i * 2) < 0) {
-            perror("pipe");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    // Execute each command
-    for (int i = 0; i < argc - 1; i++) {
-        pid = fork();
-        if (pid < 0) {
+    if (argc == 2) {
+        // Execute directly if only one command is passed
+        pid_t cpid = fork();
+        if (cpid < 0) {
             perror("fork");
             exit(EXIT_FAILURE);
         }
 
-        if (pid == 0) { // Child process
-            if (i > 0) {
-                dup2(pipefds[(i - 1) * 2], 0); // Set up read end of pipe
+        if (cpid == 0) {
+            // Child process
+            if (execlp(argv[1], argv[1], NULL) < 0) {
+                perror("execlp");
+                exit(EXIT_FAILURE);
+            }
+        } else {
+            // Parent process waits for child to finish
+            int status;
+            waitpid(cpid, &status, 0);
+            exit(WEXITSTATUS(status));
+        }
+    } else {
+        // Handle multiple commands
+        int fds[2];
+        int prev_fd = -1; // File descriptor for the read end of the previous pipe
+
+        for (int i = 1; i < argc; ++i) {
+            if (i < argc - 1) {
+                // Create a pipe for each pair of commands
+                if (pipe(fds) == -1) {
+                    perror("pipe");
+                    exit(EXIT_FAILURE);
+                }
             }
 
-            if (i < argc - 2) {
-                dup2(pipefds[i * 2 + 1], 1); // Set up write end of pipe
+            pid_t pid = fork();
+            if (pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
             }
 
-            // Close all pipe file descriptors
-            for (int j = 0; j < 2 * (argc - 2); j++) {
-                close(pipefds[j]);
-            }
+            if (pid == 0) {
+                // Child process
+                if (prev_fd != -1) {
+                    dup2(prev_fd, STDIN_FILENO);
+                    close(prev_fd);
+                }
+                if (i < argc - 1) {
+                    close(fds[0]); // Close unused read end
+                    dup2(fds[1], STDOUT_FILENO);
+                    close(fds[1]); // Close write end after duplicating
+                }
+                execlp(argv[i], argv[i], NULL);
+                // Only reached if execlp fails
+                perror("execlp");
+                exit(EXIT_FAILURE);
+            } else {
+                // Parent process
+                if (prev_fd != -1) {
+                    close(prev_fd);
+                }
+                if (i < argc - 1) {
+                    close(fds[1]); // Close unused write end
+                    prev_fd = fds[0]; // Save read end for next iteration
+                }
 
-            execlp(argv[i + 1], argv[i + 1], NULL);
-            perror("execlp");
-            exit(EXIT_FAILURE);
+                int status;
+                waitpid(pid, &status, 0);
+                if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+                    exit(EXIT_FAILURE); // Exit if child process fails
+                }
+            }
         }
     }
-
-    // Close all pipe file descriptors in parent
-    for (int i = 0; i < 2 * (argc - 2); i++) {
-        close(pipefds[i]);
-    }
-
-    // Wait for all child processes
-    while ((pid = wait(NULL)) != -1);
 
     return 0;
 }
