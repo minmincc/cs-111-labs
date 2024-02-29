6c041d6332e14c73bbb353669cfbef3cc32feec2
Mingqi Zhao <mingqizhao@Mingqis-MacBook-Air.local>
2024-02-28 18:21:37 -0800

    test1

--- lab3/hash-table-v2.c
+++ lab3/hash-table-v2.c
@@ -17,6 +17,7 @@ SLIST_HEAD(list_head, list_entry);
 
 struct hash_table_entry {
 	struct list_head list_head;
+	pthread_mutex_t mutex;
 };
 
 struct hash_table_v2 {
@@ -29,6 +30,7 @@ struct hash_table_v2 *hash_table_v2_create()
 	assert(hash_table != NULL);
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
+		pthread_mutex_init(&entry->mutex, NULL);
 		SLIST_INIT(&entry->list_head);
 	}
 	return hash_table;
@@ -73,19 +75,20 @@ void hash_table_v2_add_entry(struct hash_table_v2 *hash_table,
                              uint32_t value)
 {
 	struct hash_table_entry *hash_table_entry = get_hash_table_entry(hash_table, key);
+	pthread_mutex_lock(&hash_table_entry->mutex);
 	struct list_head *list_head = &hash_table_entry->list_head;
 	struct list_entry *list_entry = get_list_entry(hash_table, key, list_head);
 
 	/* Update the value if it already exists */
-	if (list_entry != NULL) {
-		list_entry->value = value;
-		return;
-	}
-
-	list_entry = calloc(1, sizeof(struct list_entry));
-	list_entry->key = key;
-	list_entry->value = value;
-	SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+    if (list_entry != NULL) {
+        list_entry->value = value;
+    } else {
+        list_entry = calloc(1, sizeof(struct list_entry));
+        list_entry->key = key;
+        list_entry->value = value;
+        SLIST_INSERT_HEAD(list_head, list_entry, pointers);
+    }
+    pthread_mutex_unlock(&hash_table_entry->mutex); 
 }
 
 uint32_t hash_table_v2_get_value(struct hash_table_v2 *hash_table,
@@ -102,6 +105,7 @@ void hash_table_v2_destroy(struct hash_table_v2 *hash_table)
 {
 	for (size_t i = 0; i < HASH_TABLE_CAPACITY; ++i) {
 		struct hash_table_entry *entry = &hash_table->entries[i];
+		pthread_mutex_destroy(&entry->mutex);
 		struct list_head *list_head = &entry->list_head;
 		struct list_entry *list_entry = NULL;
 		while (!SLIST_EMPTY(list_head)) {
